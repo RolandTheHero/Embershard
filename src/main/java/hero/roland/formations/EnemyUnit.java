17037d2e5b755edfbbba8d25728441aa78d24c1b
RolandTheHero <roland.sze@outlook.com>
2026-01-09 13:43:09 +1300

    Added many more units

--- src/main/java/hero/roland/formations/EnemyUnit.java
+++ src/main/java/hero/roland/formations/EnemyUnit.java
@@ -71,12 +71,242 @@ public enum EnemyUnit {
         @Override public int x() { return 53; }
         @Override public int y() { return 123; }
     },
+    WOLF_ENFORCER {
+        @Override public String filePathString() { return super.filePathString() + "SWEnforcer_front.png"; }
+        @Override public int x() { return 56; }
+        @Override public int y() { return 127; }
+    },
+    WOLF_GRENADIER {
+        @Override public String filePathString() { return super.filePathString() + "SWGrenadier_front.png"; }
+        @Override public int x() { return 34; }
+        @Override public int y() { return 112; }
+    },
+    WOLF_THUG {
+        @Override public String filePathString() { return super.filePathString() + "SWThug_front.png"; }
+        @Override public int x() { return 41; }
+        @Override public int y() { return 120; }
+    },
+    WOLF_ARMORED_TRUCK {
+        @Override public String filePathString() { return super.filePathString() + "SWarmoredTruck_front.png"; }
+        @Override public int x() { return 57; }
+        @Override public int y() { return 125; }
+    },
+    WOLF_SCRAPPER {
+        @Override public String filePathString() { return super.filePathString() + "SWscrapper_front.png"; }
+        @Override public int x() { return 52; }
+        @Override public int y() { return 133; }
+    },
+    SNIPER {
+        @Override public String filePathString() { return super.filePathString() + "Sniper2_front.png"; }
+        @Override public int x() { return 50; }
+        @Override public int y() { return 109; }
+    },
+    SUPER_TANK {
+        @Override public String filePathString() { return super.filePathString() + "Supertank_fr.png"; }
+        @Override public int x() { return 78; }
+        @Override public int y() { return 141; }
+    },
+    MEERKAT {
+        @Override public String filePathString() { return super.filePathString() + "TOW_front.png"; }
+        @Override public int x() { return 51; }
+        @Override public int y() { return 114; }
+    },
+    FLYING_DEXTER_FRAGMENT {
+        @Override public String filePathString() { return super.filePathString() + "Air_ancient_fragment_front.png"; }
+        @Override public int x() { return 96; }
+        @Override public int y() { return 258; }
+    },
+    UH1B_SKYRAPTOR {
+        @Override public String filePathString() { return super.filePathString() + "Air_attack_helicopter_front.png"; }
+        @Override public int x() { return 75; }
+        @Override public int y() { return 172; }
+    },
+    Z218_BOMBER {
+        @Override public String filePathString() { return super.filePathString() + "Air_bomber_front.png"; }
+        @Override public int x() { return 84; }
+        @Override public int y() { return 185; }
+    },
+    THUNDERCLOUD_F10 {
+        @Override public String filePathString() { return super.filePathString() + "Air_corsair_front.png"; }
+        @Override public int x() { return 63; }
+        @Override public int y() { return 136; }
+    },
+    V14_MAVERICK {
+        @Override public String filePathString() { return super.filePathString() + "Air_jet_fighter_front.png"; }
+        @Override public int x() { return 69; }
+        @Override public int y() { return 156; }
+    },
+    FALCONS_NEST {
+        @Override public String filePathString() { return super.filePathString() + "Air_raider_zeppelin_front.png"; }
+        @Override public int x() { return 99; }
+        @Override public int y() { return 181; }
+    },
+    PROTO_HUNTER {
+        @Override public String filePathString() { return super.filePathString() + "Air_s_trooper_zombie_front.png"; }
+        @Override public int x() { return 49; }
+        @Override public int y() { return 192; }
+    },
+    ADVANCED_HUNTER {
+        @Override public String filePathString() { return super.filePathString() + "Air_s_trooper_zombie_c_front.png"; }
+        @Override public int x() { return 49; }
+        @Override public int y() { return 192; }
+    },
+    ARCHETYPE_HUNTER {
+        @Override public String filePathString() { return super.filePathString() + "Air_s_trooper_zombie_d_front.png"; }
+        @Override public int x() { return 49; }
+        @Override public int y() { return 192; }
+    },
+    SPIDERWASP_DRONE {
+        @Override public String filePathString() { return super.filePathString() + "Air_spiderwasp_striker_front.png"; }
+        @Override public int x() { return 63; }
+        @Override public int y() { return 190; }
+    },
+    ROYAL_SPIDERWASP_DRONE {
+        @Override public String filePathString() { return super.filePathString() + "Air_spiderwasp_striker_70_front.png"; }
+        @Override public int x() { return 63; }
+        @Override public int y() { return 189; }
+    },
+    SILVERWOLF_CROP_BUSTER {
+        @Override public String filePathString() { return super.filePathString() + "Air_sw_plane_front.png"; }
+        @Override public int x() { return 83; }
+        @Override public int y() { return 155; }
+    },
+    SILVERWOLF_CHUNOOK_HELICOPTER {
+        @Override public String filePathString() { return super.filePathString() + "Air_sw_whirlybird_front.png"; }
+        @Override public int x() { return 65; }
+        @Override public int y() { return 219; }
+    },
+    B10_WILD_BOAR {
+        @Override public String filePathString() { return super.filePathString() + "Air_warthog_front.png"; }
+        @Override public int x() { return 70; }
+        @Override public int y() { return 150; }
+    },
+    L60_HAWKER_HURRICANE {
+        @Override public String filePathString() { return super.filePathString() + "Air_ww2_fighter_front.png"; }
+        @Override public int x() { return 67; }
+        @Override public int y() { return 149; }
+    },
+    ARMORED_FORTRESS {
+        @Override public String filePathString() { return super.filePathString() + "ArmoredFortress_front.png"; }
+        @Override public int x() { return 76; }
+        @Override public int y() { return 170; }
+    },
+    ARMORED_RAIDER_MAMMOTH {
+        @Override public String filePathString() { return super.filePathString() + "ArmoredMammoth_front.png"; }
+        @Override public int x() { return 100; }
+        @Override public int y() { return 200; }
+    },
+    ARMORED_PILLBOX {
+        @Override public String filePathString() { return super.filePathString() + "ArmoredPillbox.png"; }
+        @Override public int x() { return 49; }
+        @Override public int y() { return 110; }
+    },
+    ARSONIST {
+        @Override public String filePathString() { return super.filePathString() + "Arsonist_front.png"; }
+        @Override public int x() { return 42; }
+        @Override public int y() { return 148; }
+    },
+    LIGHT_ARTILLERY {
+        @Override public String filePathString() { return super.filePathString() + "Artillery_grey_front.png"; }
+        @Override public int x() { return 106; }
+        @Override public int y() { return 121; }
+    },
+    ASSASSINATOR {
+        @Override public String filePathString() { return super.filePathString() + "Assassinator_front.png"; }
+        @Override public int x() { return 69; }
+        @Override public int y() { return 129; }
+    },
+    BAZOOKA_SQUAD {
+        @Override public String filePathString() { return super.filePathString() + "BazookaTeam_front.png"; }
+        @Override public int x() { return 67; }
+        @Override public int y() { return 114; }
+    },
+    BIO_GRENADIER {
+        @Override public String filePathString() { return super.filePathString() + "BioGrenadier_front.png"; }
+        @Override public int x() { return 43; }
+        @Override public int y() { return 119; }
+    },
+    BLOCKHOUSE {
+        @Override public String filePathString() { return super.filePathString() + "Blockhouses_idle_front.png"; }
+        @Override public int x() { return 55; }
+        @Override public int y() { return 142; }
+    },
+    MULTILAUNCH_ROCKET_SYSTEM_LEFT {
+        @Override public String filePathString() { return super.filePathString() + "Boss_goliath_tank_leftside_front.png"; }
+        @Override public int x() { return 77; }
+        @Override public int y() { return 318; }
+    },
+    GOLIATH_TANK {
+        @Override public String filePathString() { return super.filePathString() + "Boss_goliath_tank_main_front.png"; }
+        @Override public int x() { return 122; }
+        @Override public int y() { return 380; }
+    },
+    MULTILAUNCH_ROCKET_SYSTEM_RIGHT {
+        @Override public String filePathString() { return super.filePathString() + "Boss_goliath_tank_rightside_front.png"; }
+        @Override public int x() { return 37; }
+        @Override public int y() { return 266; }
+    },
+    THE_KRAKEN {
+        @Override public String filePathString() { return super.filePathString() + "Boss_kraken_body_Crop.png"; }
+        @Override public int x() { return 151; }
+        @Override public int y() { return 161; }
+    },
+    KRAKEN_TENTACLE {
+        @Override public String filePathString() { return super.filePathString() + "Boss_kraken_arm_front_Crop.png"; }
+        @Override public int x() { return 40; }
+        @Override public int y() { return 206; }
+    },
+    DREADNOUGHT {
+        @Override public String filePathString() { return super.filePathString() + "Boss_ship_dreadnought_front.png"; }
+        @Override public int x() { return 107; }
+        @Override public int y() { return 313; }
+    },
+    SPIDERWASP_QUEEN {
+        @Override public String filePathString() { return super.filePathString() + "Boss_spiderwasp_queen_front.png"; }
+        @Override public int x() { return 134; }
+        @Override public int y() { return 237; }
+    },
+    WOLF_CRUEL_BUS {
+        @Override public String filePathString() { return super.filePathString() + "CannonSchoolBus_front.png"; }
+        @Override public int x() { return 58; }
+        @Override public int y() { return 146; }
+    },
+    RAIDER_CANNONEER {
+        @Override public String filePathString() { return super.filePathString() + "Cannonraider_front.png"; }
+        @Override public int x() { return 54; }
+        @Override public int y() { return 137; }
+    },
+    RAIDER_CHAMPION {
+        @Override public String filePathString() { return super.filePathString() + "Championraider_front.png"; }
+        @Override public int x() { return 64; }
+        @Override public int y() { return 137; }
+    },
+    CHEMICAL_SPRINKLER {
+        @Override public String filePathString() { return super.filePathString() + "ChemSprinkler_front.png"; }
+        @Override public int x() { return 42; }
+        @Override public int y() { return 156; }
+    },
+    CHEM_TROOPER {
+        @Override public String filePathString() { return super.filePathString() + "ChemTrooper_front.png"; }
+        @Override public int x() { return 50; }
+        @Override public int y() { return 131; }
+    },
+    RANGER {
+        @Override public String filePathString() { return super.filePathString() + "Commando_grey_front.png"; }
+        @Override public int x() { return 35; }
+        @Override public int y() { return 124; }
+    },
+    PEACE_KEEPER {
+        @Override public String filePathString() { return super.filePathString() + "CrowdControl_front.png"; }
+        @Override public int x() { return 53; }
+        @Override public int y() { return 123; }
+    },
 
 
     DRAGON {
         @Override public String filePathString() { return super.filePathString() + "Veh_tank_flame_heavy_front.png"; }
-        @Override public int x() { return 67; }
-        @Override public int y() { return 142; }
+        @Override public int x() { return 66; }
+        @Override public int y() { return 147; }
     },
     PUMA {
         @Override public String filePathString() { return super.filePathString() + "WheeledTank_front.png"; }
@@ -102,6 +332,53 @@ public enum EnemyUnit {
             case "SWBombadier" -> WOLF_BOMBADIER;
             case "SWDefender" -> WOLF_DEFENDER;
             case "SWDevastator" -> WOLF_DEVASTATOR;
+            case "SWEnforcer" -> WOLF_ENFORCER;
+            case "SWGrenadier" -> WOLF_GRENADIER;
+            case "SWThug" -> WOLF_THUG;
+            case "SWarmoredTruck" -> WOLF_ARMORED_TRUCK;
+            case "SWscrapper" -> WOLF_SCRAPPER;
+            case "Sniper2_front_dle" -> SNIPER;
+            case "Supertank" -> SUPER_TANK;
+            case "TOW" -> MEERKAT;
+            case "air_ancient_fragment" -> FLYING_DEXTER_FRAGMENT;
+            case "air_attack_helicopter" -> UH1B_SKYRAPTOR;
+            case "air_bomber" -> Z218_BOMBER;
+            case "air_corsair" -> THUNDERCLOUD_F10;
+            case "air_jet_fighter" -> V14_MAVERICK;
+            case "air_raider_zeppelin" -> FALCONS_NEST;
+            case "air_s_trooper_zombie" -> PROTO_HUNTER;
+            case "air_s_trooper_zombie_b" -> null;
+            case "air_s_trooper_zombie_c" -> ADVANCED_HUNTER;
+            case "air_s_trooper_zombie_d" -> ARCHETYPE_HUNTER;
+            case "air_spiderwasp_striker" -> SPIDERWASP_DRONE;
+            case "air_spiderwasp_striker_70" -> ROYAL_SPIDERWASP_DRONE;
+            case "air_sw_plane" -> SILVERWOLF_CROP_BUSTER;
+            case "air_sw_whirlybird" -> SILVERWOLF_CHUNOOK_HELICOPTER;
+            case "air_warthog" -> B10_WILD_BOAR;
+            case "air_ww2_fighter" -> L60_HAWKER_HURRICANE;
+            case "armoredFortress" -> ARMORED_FORTRESS;
+            case "armoredMammoth" -> ARMORED_RAIDER_MAMMOTH;
+            case "armoredPillbox" -> ARMORED_PILLBOX;
+            case "arsonist" -> ARSONIST;
+            case "artillery_grey" -> LIGHT_ARTILLERY;
+            case "assassinator" -> ASSASSINATOR;
+            case "bazookaTeam" -> BAZOOKA_SQUAD;
+            case "bioGrenadier" -> BIO_GRENADIER;
+            case "blockhouses_idle" -> BLOCKHOUSE;
+            case "boss_goliath_tank_leftside" -> MULTILAUNCH_ROCKET_SYSTEM_LEFT;
+            case "boss_goliath_tank_main" -> GOLIATH_TANK;
+            case "boss_goliath_tank_rightside" -> MULTILAUNCH_ROCKET_SYSTEM_RIGHT;
+            case "boss_kraken_arm" -> KRAKEN_TENTACLE;
+            case "boss_kraken_body" -> THE_KRAKEN;
+            case "boss_ship_dreadnought" -> DREADNOUGHT;
+            case "boss_spiderwasp_queen" -> SPIDERWASP_QUEEN;
+            case "cannonSchoolBus" -> WOLF_CRUEL_BUS;
+            case "cannonraider" -> RAIDER_CANNONEER;
+            case "championraider" -> RAIDER_CHAMPION;
+            case "chemSprinkler" -> CHEMICAL_SPRINKLER;
+            case "chemTrooper" -> CHEM_TROOPER;
+            case "commando_grey" -> RANGER;
+            case "crowdControl" -> PEACE_KEEPER;
 
             case "veh_tank_flame_heavy" -> DRAGON;
             case "wheeledTank" -> PUMA;
